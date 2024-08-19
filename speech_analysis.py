f9591d65bc9f201045021c120fb751bb436d38d0
yuvraj <yuvrajagarwal48@gmail.com>
2024-08-19 20:36:34 +0530

    modified nltk download

--- speech_analysis.py
+++ speech_analysis.py
@@ -27,6 +27,7 @@ import nltk
 
 
 nltk.download('punkt',quiet=True)
+nltk.download('punkt_tab',quiet=True)
 nltk.download('averaged_perceptron_tagger',quiet=True)
 
 # Function to read transcript from DOCX file
